a909937aee3be6b7692890f3a19474fa6973df76
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2023-11-28 00:53:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from streamlit_login_auth_ui.widgets import __login__
 
 __login__obj = __login__(auth_token = "courier_auth_token",
-                    company_name = "Shims",
+                    company_name = "nuse",
                     width = 200, height = 250,
                     logout_button_name = 'Logout', hide_menu_bool = False,
                     hide_footer_bool = False,
-                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+                    lottie_url = 'https://lottie.host/71c0e6d1-09be-428c-803e-86aa3250bf09/wCf0gGXTjk.json')
 
 LOGGED_IN= __login__obj.build_login_ui()
 username= __login__obj.get_username()
